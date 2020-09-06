5868997edd73eb1be49dd1bf364f8752cb02681e
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-06 12:47:33 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources: songs, only: [:new, :create, :show, :edit, :update]
+  resources :songs, only: [:index, :new, :create, :show, :edit, :update]
 end
