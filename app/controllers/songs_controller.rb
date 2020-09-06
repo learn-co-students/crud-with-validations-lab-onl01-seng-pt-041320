d782f60cd2de3764131b4dffb19b470cc27b7310
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-06 13:12:11 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,7 +7,28 @@ class SongsController < ApplicationController
         @song = Song.find(params[:id])
     end
 
+    def new
+        @song = Song.new
+    end
+
+    def create
+        @song = Song.create(song_params)
+
+        if @song.valid?
+            @song.save
+            redirect_to song_path(@song)
+        else
+            render :new
+        end
+    end
+
     def edit
         @song = Song.find(params[:id])
+    end
+
+    private
+
+    def song_params
+        params.permit(:title, :artist_name, :genre, :release_year, :released)
     end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,9 @@
 class Song < ApplicationRecord
-    validates :title, presence: true
+    validates :title, presence: true, uniqueness: {scope: release_year}
+    validates :artist_name, presence: true
+    validates :genre, presence: true
+    
+    with_options if: :self.released? do |r|
+        r.validates :release_year, presence: true, numericality: {less_than: Date.current.year}
+    end
 end
