965e69ef7f62f5d2bf20bcdb68aa9757a7fe61d0
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-08 19:46:32 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,11 +26,18 @@ class SongsController < ApplicationController
     end
 
     def update 
+        set_song
         if @song.update(song_params)
             redirect_to song_path(@song)
         else
             render :edit
         end
+    end
+
+    def destroy
+        set_song
+        @song.destroy
+        redirect_to songs_path
     end
 
     private
@@ -39,6 +46,6 @@ class SongsController < ApplicationController
     end
 
     def song_params
-        params.permit(:title, :released, :release_year, :artist_name, :genre)
+        params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
     end
 end
--- app/models/Song.rb
+++ app/models/Song.rb
@@ -1,4 +1,16 @@
 class Song < ActiveRecord::Base
     validates :title, presence: true
-    validates :title, uniqueness: true
+    validates :title, uniqueness: { scope: [:release_year, :title],
+    message: "Cannot repeat title in same year."}
+    validates :released, inclusion: { in: [true, false] }
+    validates :release_year, presence: true, if: :released
+    validates :valid_release_year
+
+    private
+    def valid_release_year
+        if self.release_year > Date.today.year
+            self.errors.add(:release_year, "Must be older or current year")
+        end
+    end
+    
 end
